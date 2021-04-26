a9cc6b43336ac7b9ff7795c72e5a4a459da0daa2
PaulaSand <p.l.costa.1989@gmail.com>
2021-04-25 23:08:16 -0300

    cuartoCommit-headerSinTextStyle

--- notePad.bat
+++ notePad.bat
@@ -1,1 +1,26 @@
 hola como estas
+background-size: 100%;
+  background-position: left;
+  background-position-x: -400px ;
+  background-position-y:-400px ;
+  background-repeat: no-repeat;
+  padding: 50px;
+
+  background-size: 100%;
+  background-position: left;
+  background-position-x: -400px ;
+  background-position-y:-400px ;
+  background-repeat: no-repeat;
+  padding: 50px;
+
+@import url('https://fonts.googleapis.com/css2?family=Overpass:wght@300;600&display=swap');
+</style>
+CSS rules to specify families
+
+font-family: 'Overpass', sans-serif;
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Overpass:wght@300;600&display=swap" rel="stylesheet">
+CSS rules to specify families
+
+font-family: 'Overpass', sans-serif;
